9a634e2fc829520ebf50f58be90f9ff7bbe92b11
Michael Fienen <fienen@gmail.com>
2017-08-21 00:01:58 -0500

    Add header/footer theme files

---
+++ header.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+  <title></title>
+
+  <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>">
+  <script src="<?php echo get_template_directory_uri(); ?>/js/vendors.min.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/custom.min.js"></script>
+
+  <?php wp_head(); ?>
+</head>
+<body>
