335c81151203ca7812502912a55cecf18fa2b55f
Michael Fienen <fienen@gmail.com>
2017-09-08 21:17:29 -0500

    Add more CPTs

--- includes/data/post-types.php
+++ includes/data/post-types.php
@@ -79,7 +79,7 @@ function tnw_cpt_cyphers() {
 		'set_featured_image'    => __( 'Set featured image', 'the-ninth-world' ),
 		'remove_featured_image' => __( 'Remove featured image', 'the-ninth-world' ),
 		'use_featured_image'    => __( 'Use as featured image', 'the-ninth-world' ),
-		'insert_into_item'      => __( 'Insert into item', 'the-ninth-world' ),
+		'insert_into_item'      => __( 'Insert into Descriptor', 'the-ninth-world' ),
 		'uploaded_to_this_item' => __( 'Uploaded to this cypher', 'the-ninth-world' ),
 		'items_list'            => __( 'Cyphers list', 'the-ninth-world' ),
 		'items_list_navigation' => __( 'Cyphers list navigation', 'the-ninth-world' ),
@@ -107,3 +107,220 @@ function tnw_cpt_cyphers() {
 	register_post_type( 'cyphers', $args );
 }
 add_action( 'init', 'tnw_cpt_cyphers', 0 );
+
+// Register Descriptor Post Type
+function tnw_cpt_descriptors() {
+	$labels = array(
+		'name'                  => _x( 'Descriptors', 'Post Type General Name', 'the-ninth-world' ),
+		'singular_name'         => _x( 'Descriptor', 'Post Type Singular Name', 'the-ninth-world' ),
+		'menu_name'             => __( 'Descriptors', 'the-ninth-world' ),
+		'name_admin_bar'        => __( 'Descriptor', 'the-ninth-world' ),
+		'archives'              => __( 'Descriptor Archives', 'the-ninth-world' ),
+		'attributes'            => __( 'Descriptor Attributes', 'the-ninth-world' ),
+		'parent_item_colon'     => __( 'Parent Descriptor:', 'the-ninth-world' ),
+		'all_items'             => __( 'All Descriptors', 'the-ninth-world' ),
+		'add_new_item'          => __( 'Add New Descriptor', 'the-ninth-world' ),
+		'add_new'               => __( 'Add New Descriptor', 'the-ninth-world' ),
+		'new_item'              => __( 'New Descriptor', 'the-ninth-world' ),
+		'edit_item'             => __( 'Edit Descriptor', 'the-ninth-world' ),
+		'update_item'           => __( 'Update Descriptor', 'the-ninth-world' ),
+		'view_item'             => __( 'View Descriptor', 'the-ninth-world' ),
+		'view_items'            => __( 'View Descriptors', 'the-ninth-world' ),
+		'search_items'          => __( 'Search Descriptor', 'the-ninth-world' ),
+		'not_found'             => __( 'Not found', 'the-ninth-world' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'the-ninth-world' ),
+		'featured_image'        => __( 'Featured Image', 'the-ninth-world' ),
+		'set_featured_image'    => __( 'Set featured image', 'the-ninth-world' ),
+		'remove_featured_image' => __( 'Remove featured image', 'the-ninth-world' ),
+		'use_featured_image'    => __( 'Use as featured image', 'the-ninth-world' ),
+		'insert_into_item'      => __( 'Insert into Descriptor', 'the-ninth-world' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this Descriptor', 'the-ninth-world' ),
+		'items_list'            => __( 'Descriptors list', 'the-ninth-world' ),
+		'items_list_navigation' => __( 'Descriptors list navigation', 'the-ninth-world' ),
+		'filter_items_list'     => __( 'Filter Descriptors list', 'the-ninth-world' ),
+	);
+	$args = array(
+		'label'                 => __( 'Descriptor', 'the-ninth-world' ),
+		'description'           => __( 'Character Descriptor types', 'the-ninth-world' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'author', 'thumbnail', 'revisions', ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-id-alt',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'post',
+	);
+	register_post_type( 'descriptors', $args );
+}
+add_action( 'init', 'tnw_cpt_descriptors', 0 );
+
+// Register Encounter Post Type
+function tnw_cpt_encounters() {
+	$labels = array(
+		'name'                  => _x( 'Encounters', 'Post Type General Name', 'the-ninth-world' ),
+		'singular_name'         => _x( 'Encounter', 'Post Type Singular Name', 'the-ninth-world' ),
+		'menu_name'             => __( 'Encounters', 'the-ninth-world' ),
+		'name_admin_bar'        => __( 'Encounter', 'the-ninth-world' ),
+		'archives'              => __( 'Encounter Archives', 'the-ninth-world' ),
+		'attributes'            => __( 'Encounter Attributes', 'the-ninth-world' ),
+		'parent_item_colon'     => __( 'Parent Encounter:', 'the-ninth-world' ),
+		'all_items'             => __( 'All Encounters', 'the-ninth-world' ),
+		'add_new_item'          => __( 'Add New Encounter', 'the-ninth-world' ),
+		'add_new'               => __( 'Add New Encounter', 'the-ninth-world' ),
+		'new_item'              => __( 'New Encounter', 'the-ninth-world' ),
+		'edit_item'             => __( 'Edit Encounter', 'the-ninth-world' ),
+		'update_item'           => __( 'Update Encounter', 'the-ninth-world' ),
+		'view_item'             => __( 'View Encounter', 'the-ninth-world' ),
+		'view_items'            => __( 'View Encounters', 'the-ninth-world' ),
+		'search_items'          => __( 'Search Encounter', 'the-ninth-world' ),
+		'not_found'             => __( 'Not found', 'the-ninth-world' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'the-ninth-world' ),
+		'featured_image'        => __( 'Featured Image', 'the-ninth-world' ),
+		'set_featured_image'    => __( 'Set featured image', 'the-ninth-world' ),
+		'remove_featured_image' => __( 'Remove featured image', 'the-ninth-world' ),
+		'use_featured_image'    => __( 'Use as featured image', 'the-ninth-world' ),
+		'insert_into_item'      => __( 'Insert into Encounter', 'the-ninth-world' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this Encounter', 'the-ninth-world' ),
+		'items_list'            => __( 'Encounters list', 'the-ninth-world' ),
+		'items_list_navigation' => __( 'Encounters list navigation', 'the-ninth-world' ),
+		'filter_items_list'     => __( 'Filter Encounters list', 'the-ninth-world' ),
+	);
+	$args = array(
+		'label'                 => __( 'Encounter', 'the-ninth-world' ),
+		'description'           => __( 'Character Encounter types', 'the-ninth-world' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'author', 'thumbnail', 'revisions', ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-groups',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'post',
+	);
+	register_post_type( 'encounters', $args );
+}
+add_action( 'init', 'tnw_cpt_encounters', 0 );
+
+// Register Equipment Post Type
+function tnw_cpt_equipment() {
+	$labels = array(
+		'name'                  => _x( 'Equipment', 'Post Type General Name', 'the-ninth-world' ),
+		'singular_name'         => _x( 'Equipment', 'Post Type Singular Name', 'the-ninth-world' ),
+		'menu_name'             => __( 'Equipment', 'the-ninth-world' ),
+		'name_admin_bar'        => __( 'Equipment', 'the-ninth-world' ),
+		'archives'              => __( 'Equipment Archives', 'the-ninth-world' ),
+		'attributes'            => __( 'Equipment Attributes', 'the-ninth-world' ),
+		'parent_item_colon'     => __( 'Parent Equipment:', 'the-ninth-world' ),
+		'all_items'             => __( 'All Equipment', 'the-ninth-world' ),
+		'add_new_item'          => __( 'Add New Equipment', 'the-ninth-world' ),
+		'add_new'               => __( 'Add New Equipment', 'the-ninth-world' ),
+		'new_item'              => __( 'New Equipment', 'the-ninth-world' ),
+		'edit_item'             => __( 'Edit Equipment', 'the-ninth-world' ),
+		'update_item'           => __( 'Update Equipment', 'the-ninth-world' ),
+		'view_item'             => __( 'View Equipment', 'the-ninth-world' ),
+		'view_items'            => __( 'View Equipment', 'the-ninth-world' ),
+		'search_items'          => __( 'Search Equipment', 'the-ninth-world' ),
+		'not_found'             => __( 'Not found', 'the-ninth-world' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'the-ninth-world' ),
+		'featured_image'        => __( 'Featured Image', 'the-ninth-world' ),
+		'set_featured_image'    => __( 'Set featured image', 'the-ninth-world' ),
+		'remove_featured_image' => __( 'Remove featured image', 'the-ninth-world' ),
+		'use_featured_image'    => __( 'Use as featured image', 'the-ninth-world' ),
+		'insert_into_item'      => __( 'Insert into Equipment', 'the-ninth-world' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this Equipment', 'the-ninth-world' ),
+		'items_list'            => __( 'Equipment list', 'the-ninth-world' ),
+		'items_list_navigation' => __( 'Equipment list navigation', 'the-ninth-world' ),
+		'filter_items_list'     => __( 'Filter Equipment list', 'the-ninth-world' ),
+	);
+	$args = array(
+		'label'                 => __( 'Equipment', 'the-ninth-world' ),
+		'description'           => __( 'Equipment item types', 'the-ninth-world' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'author', 'thumbnail', 'revisions', ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-shield-alt',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'post',
+	);
+	register_post_type( 'equipment', $args );
+}
+add_action( 'init', 'tnw_cpt_equipment', 0 );
+
+// Register Foci Post Type
+function tnw_cpt_foci() {
+	$labels = array(
+		'name'                  => _x( 'Foci', 'Post Type General Name', 'the-ninth-world' ),
+		'singular_name'         => _x( 'Focus', 'Post Type Singular Name', 'the-ninth-world' ),
+		'menu_name'             => __( 'Foci', 'the-ninth-world' ),
+		'name_admin_bar'        => __( 'Focus', 'the-ninth-world' ),
+		'archives'              => __( 'Focus Archives', 'the-ninth-world' ),
+		'attributes'            => __( 'Focus Attributes', 'the-ninth-world' ),
+		'parent_item_colon'     => __( 'Parent Focus:', 'the-ninth-world' ),
+		'all_items'             => __( 'All Foci', 'the-ninth-world' ),
+		'add_new_item'          => __( 'Add New Focus', 'the-ninth-world' ),
+		'add_new'               => __( 'Add New Focus', 'the-ninth-world' ),
+		'new_item'              => __( 'New Focus', 'the-ninth-world' ),
+		'edit_item'             => __( 'Edit Focus', 'the-ninth-world' ),
+		'update_item'           => __( 'Update Focus', 'the-ninth-world' ),
+		'view_item'             => __( 'View Focus', 'the-ninth-world' ),
+		'view_items'            => __( 'View Foci', 'the-ninth-world' ),
+		'search_items'          => __( 'Search Focus', 'the-ninth-world' ),
+		'not_found'             => __( 'Not found', 'the-ninth-world' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'the-ninth-world' ),
+		'featured_image'        => __( 'Featured Image', 'the-ninth-world' ),
+		'set_featured_image'    => __( 'Set featured image', 'the-ninth-world' ),
+		'remove_featured_image' => __( 'Remove featured image', 'the-ninth-world' ),
+		'use_featured_image'    => __( 'Use as featured image', 'the-ninth-world' ),
+		'insert_into_item'      => __( 'Insert into Focus', 'the-ninth-world' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this Focus', 'the-ninth-world' ),
+		'items_list'            => __( 'Foci list', 'the-ninth-world' ),
+		'items_list_navigation' => __( 'Foci list navigation', 'the-ninth-world' ),
+		'filter_items_list'     => __( 'Filter Foci list', 'the-ninth-world' ),
+	);
+	$args = array(
+		'label'                 => __( 'Focus', 'the-ninth-world' ),
+		'description'           => __( 'Character Focus types', 'the-ninth-world' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'author', 'thumbnail', 'revisions', ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-album',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'post',
+	);
+	register_post_type( 'foci', $args );
+
+}
+add_action( 'init', 'tnw_cpt_foci', 0 );
