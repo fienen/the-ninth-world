f3478cfb480dbe00e6aa2f224cedbf445c0121e9
Michael Fienen <fienen@gmail.com>
2017-09-08 23:13:56 -0500

    Add taxonomy for equipment rarity

--- includes/data/taxonomies.php
+++ includes/data/taxonomies.php
@@ -1,5 +1,5 @@
 <?php
-// Register Custom Taxonomy
+// Register Custom Taxonomy for Encounters (Encounter Type)
 function tnw_tax_encounters() {
 	$labels = array(
 		'name'                       => _x( 'Encounter Types', 'Taxonomy General Name', 'the-ninth-world' ),
@@ -35,3 +35,40 @@ function tnw_tax_encounters() {
 	register_taxonomy( 'encounter-type', array( 'encounters' ), $args );
 }
 add_action( 'init', 'tnw_tax_encounters', 0 );
+
+// Register Custom Taxonomy for Equipment (Item Rarity)
+function tnw_tax_equipment() {
+	$labels = array(
+		'name'                       => _x( 'Item Rarity', 'Taxonomy General Name', 'the-ninth-world' ),
+		'singular_name'              => _x( 'Item Rarity', 'Taxonomy Singular Name', 'the-ninth-world' ),
+		'menu_name'                  => __( 'Item Rarity', 'the-ninth-world' ),
+		'all_items'                  => __( 'All Item Rarities', 'the-ninth-world' ),
+		'parent_item'                => __( 'Parent Item Rarity', 'the-ninth-world' ),
+		'parent_item_colon'          => __( 'Parent Item Rarity:', 'the-ninth-world' ),
+		'new_item_name'              => __( 'New Item Rarity', 'the-ninth-world' ),
+		'add_new_item'               => __( 'Add New Item Rarity', 'the-ninth-world' ),
+		'edit_item'                  => __( 'Edit Item Rarity', 'the-ninth-world' ),
+		'update_item'                => __( 'Update Item Rarity', 'the-ninth-world' ),
+		'view_item'                  => __( 'View Item Rarity', 'the-ninth-world' ),
+		'separate_items_with_commas' => __( 'Separate Item Rarities with commas', 'the-ninth-world' ),
+		'add_or_remove_items'        => __( 'Add or remove Item Rarity', 'the-ninth-world' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'the-ninth-world' ),
+		'popular_items'              => __( 'Popular Item Rarities', 'the-ninth-world' ),
+		'search_items'               => __( 'Search Item Rarities', 'the-ninth-world' ),
+		'not_found'                  => __( 'Not Found', 'the-ninth-world' ),
+		'no_terms'                   => __( 'No Item Rarity', 'the-ninth-world' ),
+		'items_list'                 => __( 'Item Rarities list', 'the-ninth-world' ),
+		'items_list_navigation'      => __( 'Item Rarities list navigation', 'the-ninth-world' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => false,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'item-rarity', array( 'equipment' ), $args );
+}
+add_action( 'init', 'tnw_tax_equipment', 0 );
