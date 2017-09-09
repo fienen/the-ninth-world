7bf42cf99ec8a65f02c4d16adaa9485a299b4504
Michael Fienen <fienen@gmail.com>
2017-09-08 21:54:48 -0500

    Fix meta box label

--- includes/data/meta-boxes.php
+++ includes/data/meta-boxes.php
@@ -45,7 +45,7 @@ class Rational_Meta_Box {
 		foreach ( $this->screens as $screen ) {
 			add_meta_box(
 				'advanced-options',
-				__( 'Advanced Options', 'rational-metabox' ),
+				__( 'Artifact Details', 'rational-metabox' ),
 				array( $this, 'add_meta_box_callback' ),
 				$screen,
 				'normal',
