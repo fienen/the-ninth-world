06b22a6505b0bf94dca7527917e4869999b85e49
Michael Fienen <fienen@gmail.com>
2017-09-11 07:30:17 -0500

    Create a layouts directory

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 require_once('includes/data/post-types.php');
-require_once('includes/data/meta-boxes.php');
 require_once('includes/data/taxonomies.php');
+require_once('includes/layout/meta-boxes.php');
 
 /*************************************
  Custom Shortcodes
