bc166c6f4550e8c3ca8550dedb501873724ce8bd
Michael Fienen <fienen@gmail.com>
2017-10-10 23:24:54 -0500

    Add publication meta box

--- includes/layout/meta-boxes.php
+++ includes/layout/meta-boxes.php
@@ -1370,3 +1370,154 @@ class Mutation_Meta_Box {
 	}
 }
 new Mutation_Meta_Box;
+
+class Publication_Meta_Box {
+	private $screens = array(
+		'publications',
+	);
+	private $fields = array(
+		array(
+			'id' => 'authors',
+			'label' => 'Author(s)',
+			'type' => 'text',
+			'options' => array(
+				'Beneficial' => 'Beneficial',
+				'Harmful' => 'Harmful',
+				'Powerful' => 'Powerful',
+				'Distinctive' => 'Distinctive',
+			),
+		),
+		array(
+			'id' => 'link',
+			'label' => 'Link',
+			'type' => 'url',
+		),
+		array(
+			'id' => 'availability',
+			'label' => 'Availability',
+			'type' => 'select',
+			'options' => array(
+				'soon' => 'Coming Soon',
+				'released' => 'Released',
+			),
+		),
+	);
+
+	/**
+	 * Class construct method. Adds actions to their respective WordPress hooks.
+	 */
+	public function __construct() {
+		add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) );
+		add_action( 'save_post', array( $this, 'save_post' ) );
+	}
+
+	/**
+	 * Hooks into WordPress' add_meta_boxes function.
+	 * Goes through screens (post types) and adds the meta box.
+	 */
+	public function add_meta_boxes() {
+		foreach ( $this->screens as $screen ) {
+			add_meta_box(
+				'publication-details',
+				__( 'Publication Details', 'the-ninth-world' ),
+				array( $this, 'add_meta_box_callback' ),
+				$screen,
+				'normal',
+				'high'
+			);
+		}
+	}
+
+	/**
+	 * Generates the HTML for the meta box
+	 *
+	 * @param object $post WordPress post object
+	 */
+	public function add_meta_box_callback( $post ) {
+		wp_nonce_field( 'publication_details_data', 'publication_details_nonce' );
+		$this->generate_fields( $post );
+	}
+
+	/**
+	 * Generates the field's HTML for the meta box.
+	 */
+	public function generate_fields( $post ) {
+		$output = '';
+		foreach ( $this->fields as $field ) {
+			$label = '<label for="' . $field['id'] . '">' . $field['label'] . '</label>';
+			$db_value = get_post_meta( $post->ID, 'publication_details_' . $field['id'], true );
+			switch ( $field['type'] ) {
+				case 'select':
+					$input = sprintf(
+						'<select id="%s" name="%s">',
+						$field['id'],
+						$field['id']
+					);
+					foreach ( $field['options'] as $key => $value ) {
+						$field_value = !is_numeric( $key ) ? $key : $value;
+						$input .= sprintf(
+							'<option %s value="%s">%s</option>',
+							$db_value === $field_value ? 'selected' : '',
+							$field_value,
+							$value
+						);
+					}
+					$input .= '</select>';
+					break;
+				default:
+					$input = sprintf(
+						'<input %s id="%s" name="%s" type="%s" value="%s">',
+						$field['type'] !== 'color' ? 'class="regular-text"' : '',
+						$field['id'],
+						$field['id'],
+						$field['type'],
+						$db_value
+					);
+			}
+			$output .= $this->row_format( $label, $input );
+		}
+		echo '<table class="form-table"><tbody>' . $output . '</tbody></table>';
+	}
+
+	/**
+	 * Generates the HTML for table rows.
+	 */
+	public function row_format( $label, $input ) {
+		return sprintf(
+			'<tr><th scope="row">%s</th><td>%s</td></tr>',
+			$label,
+			$input
+		);
+	}
+	/**
+	 * Hooks into WordPress' save_post function
+	 */
+	public function save_post( $post_id ) {
+		if ( ! isset( $_POST['publication_details_nonce'] ) )
+			return $post_id;
+
+		$nonce = $_POST['publication_details_nonce'];
+		if ( !wp_verify_nonce( $nonce, 'publication_details_data' ) )
+			return $post_id;
+
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+			return $post_id;
+
+		foreach ( $this->fields as $field ) {
+			if ( isset( $_POST[ $field['id'] ] ) ) {
+				switch ( $field['type'] ) {
+					case 'email':
+						$_POST[ $field['id'] ] = sanitize_email( $_POST[ $field['id'] ] );
+						break;
+					case 'text':
+						$_POST[ $field['id'] ] = sanitize_text_field( $_POST[ $field['id'] ] );
+						break;
+				}
+				update_post_meta( $post_id, 'publication_details_' . $field['id'], $_POST[ $field['id'] ] );
+			} else if ( $field['type'] === 'checkbox' ) {
+				update_post_meta( $post_id, 'publication_details_' . $field['id'], '0' );
+			}
+		}
+	}
+}
+new Publication_Meta_Box;
